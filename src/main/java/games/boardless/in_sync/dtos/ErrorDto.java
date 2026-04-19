5b0abc65ed009716f49d69120315b86b15f513a7
poold3 <dallinpool@gmail.com>
2026-04-18 23:41:19 -0400

    Converting DTOs to Records.

--- src/main/java/games/boardless/in_sync/dtos/ErrorDto.java
+++ src/main/java/games/boardless/in_sync/dtos/ErrorDto.java
@@ -1,43 +1,4 @@
 package games.boardless.in_sync.dtos;
 
-public class ErrorDto {
-  private final String error;
-
-  public ErrorDto(final String error) {
-    this.error = error;
-  }
-
-  public String getError() {
-    return this.error;
-  }
-
-  @Override
-  public String toString() {
-    return "ErrorDto [error=" + error + "]";
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((error == null) ? 0 : error.hashCode());
-    return result;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    ErrorDto other = (ErrorDto) obj;
-    if (error == null) {
-      if (other.error != null)
-        return false;
-    } else if (!error.equals(other.error))
-      return false;
-    return true;
-  }
+public record ErrorDto(String error) {
 }
