e8734c07c2666da7027bd4f29d1c1d245aad2fee
poold3 <dallinpool@gmail.com>
2026-04-18 23:42:22 -0400

    Creating new custom exceptions.

---
+++ src/main/java/games/boardless/in_sync/exceptions/ServiceUnavailableException.java
@@ -0,0 +1,19 @@
+package games.boardless.in_sync.exceptions;
+
+public class ServiceUnavailableException extends Exception {
+  public ServiceUnavailableException() {
+    super();
+  }
+
+  public ServiceUnavailableException(final String message) {
+    super(message);
+  }
+
+  public ServiceUnavailableException(final String message, final Throwable cause) {
+    super(message, cause);
+  }
+
+  public ServiceUnavailableException(final Throwable cause) {
+    super(cause);
+  }
+}
