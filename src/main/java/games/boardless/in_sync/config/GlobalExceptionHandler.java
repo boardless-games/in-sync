92080e59b727a6b73e37e74e828b830677a27072
poold3 <dallinpool@gmail.com>
2026-04-25 11:31:08 -0400

    Adding handler for MethodNotSupported

--- src/main/java/games/boardless/in_sync/config/GlobalExceptionHandler.java
+++ src/main/java/games/boardless/in_sync/config/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -26,6 +27,12 @@ public class GlobalExceptionHandler {
   public ResponseEntity<ErrorDto> handleServiceUnavailable(Exception e) {
     logger.info(e.getMessage());
     return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(new ErrorDto(e.getMessage()));
+  }
+
+  @ExceptionHandler(exception = { HttpRequestMethodNotSupportedException.class })
+  public ResponseEntity<ErrorDto> handleRequestMethodNotSupported(Exception e) {
+    logger.info(e.getMessage());
+    return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(new ErrorDto(e.getMessage()));
   }
 
   @ExceptionHandler(exception = Exception.class)
