a2be332e5cd82fc3092ace7a792a8c2b1156d1cd
poold3 <dallinpool@gmail.com>
2026-04-25 11:54:04 -0400

    Added ready to player.

--- src/main/java/games/boardless/in_sync/models/Player.java
+++ src/main/java/games/boardless/in_sync/models/Player.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.PingMessage;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
@@ -11,10 +12,12 @@ public class Player {
   private static final Logger logger = LoggerFactory.getLogger(Player.class);
   private final String name;
   private WebSocketSession session;
+  private boolean ready;
 
   public Player(final String name) {
     this.name = name;
     this.session = null;
+    this.ready = false;
   }
 
   public String getName() {
@@ -29,9 +32,13 @@ public class Player {
     return this.session != null;
   }
 
+  public boolean isReady() {
+    return this.isConnected() && this.ready;
+  }
+
   @Override
   public String toString() {
-    return "Player [name=" + name + ", sessionId=" + this.session != null ? this.session.getId() : null + "]";
+    return "Player [name=" + name + ", sessionId=" + this.session != null ? this.session.getId() : null + ", ready=" + this.ready + "]";
   }
 
   @Override
@@ -69,5 +76,23 @@ public class Player {
       logger.error(
           String.format("Failed to send message (%s) to %s.", message.getPayload(), this.toString()), e);
     }
+  }
+
+  public void ping() {
+    this.ready = false;
+    if (this.session == null)
+      return;
+    try {
+      this.session.sendMessage(new PingMessage());
+    } catch (IOException e) {
+      logger.error(
+          String.format("Failed to ping %s.", this.toString()), e);
+    }
+  }
+
+  public void pong() {
+    if (this.session == null)
+      return;
+    this.ready = true;
   }
 }
