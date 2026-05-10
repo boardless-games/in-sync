03621897eb04219712ffb75087120acda94966dd
poold3 <dallinpool@gmail.com>
2026-05-09 21:40:24 -0400

    Better logging and only handling unchecked exceptions.

--- src/main/java/games/boardless/in_sync/config/GlobalExceptionHandler.java
+++ src/main/java/games/boardless/in_sync/config/GlobalExceptionHandler.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.http.converter.HttpMessageConversionException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,30 +12,26 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import games.boardless.in_sync.dtos.ErrorDto;
 import games.boardless.in_sync.exceptions.BadRequestException;
 import games.boardless.in_sync.exceptions.ServiceUnavailableException;
+import games.boardless.in_sync.utils.ToString;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
   private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-  @ExceptionHandler(exception = { BadRequestException.class, MissingServletRequestParameterException.class })
+  @ExceptionHandler(exception = { BadRequestException.class, MissingServletRequestParameterException.class,
+      HttpMessageConversionException.class })
   public ResponseEntity<ErrorDto> handleBadRequest(Exception e) {
-    logger.info(e.getMessage());
+    logger.info(ToString.toString(e, 1));
     return ResponseEntity.badRequest().body(new ErrorDto(e.getMessage()));
   }
 
   @ExceptionHandler(exception = { ServiceUnavailableException.class })
   public ResponseEntity<ErrorDto> handleServiceUnavailable(Exception e) {
-    logger.info(e.getMessage());
+    logger.info(ToString.toString(e, 1));
     return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(new ErrorDto(e.getMessage()));
   }
 
-  @ExceptionHandler(exception = { HttpRequestMethodNotSupportedException.class })
-  public ResponseEntity<ErrorDto> handleRequestMethodNotSupported(Exception e) {
-    logger.info(e.getMessage());
-    return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(new ErrorDto(e.getMessage()));
-  }
-
-  @ExceptionHandler(exception = Exception.class)
+  @ExceptionHandler(exception = RuntimeException.class)
   public ResponseEntity<ErrorDto> handleInternalServerError(Exception e) {
     logger.error(e.getMessage(), e);
     return ResponseEntity.internalServerError()
