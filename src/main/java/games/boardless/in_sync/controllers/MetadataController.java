ee9a574b06fc2b8e3031804f877dfeb2300a254e
poold3 <dallinpool@gmail.com>
2026-05-09 21:45:50 -0400

    Removing logging on MetadataController.

--- src/main/java/games/boardless/in_sync/controllers/MetadataController.java
+++ src/main/java/games/boardless/in_sync/controllers/MetadataController.java
@@ -1,7 +1,5 @@
 package games.boardless.in_sync.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +12,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 @RestController
 @RequestMapping
 public class MetadataController {
-  private static final Logger logger = LoggerFactory.getLogger(MetadataController.class);
-
   @Operation(summary = "Returns the server's health.")
   @ApiResponse(responseCode = "200", description = "The server is up!", content = @Content)
   @GetMapping("/health")
   public ResponseEntity<Void> health() {
-    logger.info("The server is up!");
     return ResponseEntity.ok().build();
   }
 }
