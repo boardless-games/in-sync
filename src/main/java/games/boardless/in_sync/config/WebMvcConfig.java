a6e910c80953b8c91cf3ddc835f2933b1598ebb6
poold3 <dallinpool@gmail.com>
2026-05-09 21:40:50 -0400

    Adding Configuration annotation.

--- src/main/java/games/boardless/in_sync/config/WebMvcConfig.java
+++ src/main/java/games/boardless/in_sync/config/WebMvcConfig.java
@@ -2,9 +2,11 @@ package games.boardless.in_sync.config;
 
 import static games.boardless.in_sync.constants.Constants.ALLOWED_ORIGINS;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(final CorsRegistry registry) {
