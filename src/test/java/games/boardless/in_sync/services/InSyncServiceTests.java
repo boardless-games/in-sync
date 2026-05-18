cfcfb5f7f9002858d7c6c6fc2488895d1b8b5d19
poold3 <dallinpool@gmail.com>
2026-05-17 22:37:39 -0400

    Start Game test cases.

--- src/test/java/games/boardless/in_sync/services/InSyncServiceTests.java
+++ src/test/java/games/boardless/in_sync/services/InSyncServiceTests.java
@@ -3,13 +3,17 @@ package games.boardless.in_sync.services;
 import static games.boardless.in_sync.constants.Constants.GAME_AUTO_DELETE_TIME;
 import static games.boardless.in_sync.constants.Constants.MAX_NUM_GAMES;
 import static games.boardless.in_sync.constants.Constants.PLAYER_AUTO_REMOVE_TIME;
+import static games.boardless.in_sync.constants.Constants.WAIT_PLAYER_READY_TIME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
 import static org.mockito.Mockito.verify;
@@ -33,6 +37,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import games.boardless.in_sync.dtos.GameCodeDto;
 import games.boardless.in_sync.dtos.NameDto;
@@ -172,4 +177,105 @@ class InSyncServiceTests {
       assertEquals(this.clock.instant().plusMillis(PLAYER_AUTO_REMOVE_TIME), startTimeCaptor.getValue());
     }
   }
+
+  @Test
+  void startGame_withInvalidGameCode_shouldThrowBadRequestException() {
+    try (MockedStatic<InputValidation> mockInputValidation = mockStatic(InputValidation.class)) {
+      final String validationResult = UUID.randomUUID().toString();
+      mockInputValidation.when(() -> InputValidation.validateGameCode(anyString()))
+          .thenReturn(Optional.of(validationResult));
+
+      final BadRequestException e = assertThrows(BadRequestException.class, () -> {
+        this.inSyncService.startGame("");
+      });
+
+      assertEquals(validationResult, e.getMessage());
+      mockInputValidation.verify(() -> InputValidation.validateGameCode(anyString()));
+    }
+  }
+
+  @Test
+  void startGame_withNonExistentGameCode_shouldThrowBadRequestException() {
+    try (MockedStatic<InputValidation> mockInputValidation = mockStatic(InputValidation.class)) {
+      mockInputValidation.when(() -> InputValidation.validateGameCode(anyString()))
+          .thenReturn(Optional.empty());
+
+      final String gameCode = UUID.randomUUID().toString();
+      final BadRequestException e = assertThrows(BadRequestException.class, () -> {
+        this.inSyncService.startGame(gameCode);
+      });
+
+      assertTrue(e.getMessage().contains(gameCode));
+    }
+  }
+
+  @Test
+  void startGame_withBadAutoStart_shouldHaveExceptionResult() throws BadRequestException, ServiceUnavailableException {
+    try (MockedStatic<InputValidation> mockInputValidation = mockStatic(InputValidation.class)) {
+      mockInputValidation.when(() -> InputValidation.validateGameCode(anyString()))
+          .thenReturn(Optional.empty());
+
+      final String gameCode = UUID.randomUUID().toString();
+      final Game game = mock();
+      this.inSyncService.games.put(gameCode, game);
+
+      final DeferredResult<ResponseEntity<Void>> deferredResult = this.inSyncService.startGame(gameCode);
+      verify(game).initialize();
+      assertFalse(deferredResult.hasResult() || deferredResult.isSetOrExpired());
+
+      final ArgumentCaptor<Runnable> taskCaptor = ArgumentCaptor.forClass(Runnable.class);
+      final ArgumentCaptor<Instant> startTimeCaptor = ArgumentCaptor.forClass(Instant.class);
+      verify(this.taskScheduler).schedule(taskCaptor.capture(), startTimeCaptor.capture());
+
+      final String errorMessage = UUID.randomUUID().toString();
+      doThrow(new BadRequestException(errorMessage)).when(this.inSyncService).autoStartGame(anyString());
+      taskCaptor.getValue().run();
+      verify(this.inSyncService).autoStartGame(gameCode);
+      assertTrue(deferredResult.hasResult());
+      final Object result = deferredResult.getResult();
+      if (result instanceof BadRequestException) {
+        assertEquals(errorMessage, ((BadRequestException) result).getMessage());
+      } else {
+        fail("result should be an instance of BadRequestException.");
+      }
+
+      assertEquals(this.clock.instant().plusMillis(WAIT_PLAYER_READY_TIME), startTimeCaptor.getValue());
+    }
+  }
+
+  @Test
+  void startGame_shouldStartGame() throws BadRequestException, ServiceUnavailableException {
+    try (MockedStatic<InputValidation> mockInputValidation = mockStatic(InputValidation.class)) {
+      mockInputValidation.when(() -> InputValidation.validateGameCode(anyString()))
+          .thenReturn(Optional.empty());
+
+      final String gameCode = UUID.randomUUID().toString();
+      final Game game = mock();
+      this.inSyncService.games.put(gameCode, game);
+
+      final DeferredResult<ResponseEntity<Void>> deferredResult = this.inSyncService.startGame(gameCode);
+      verify(game).initialize();
+      assertFalse(deferredResult.hasResult() || deferredResult.isSetOrExpired());
+
+      final ArgumentCaptor<Runnable> taskCaptor = ArgumentCaptor.forClass(Runnable.class);
+      final ArgumentCaptor<Instant> startTimeCaptor = ArgumentCaptor.forClass(Instant.class);
+      verify(this.taskScheduler).schedule(taskCaptor.capture(), startTimeCaptor.capture());
+
+      doNothing().when(this.inSyncService).autoStartGame(anyString());
+      taskCaptor.getValue().run();
+      verify(this.inSyncService).autoStartGame(gameCode);
+      assertTrue(deferredResult.hasResult());
+      final Object result = deferredResult.getResult();
+      if (result instanceof ResponseEntity<?>) {
+        final ResponseEntity<?> responseEntity = (ResponseEntity<?>) result;
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        assertNull(responseEntity.getBody());
+      } else {
+        fail("result should be an instance of ResponseEntity.");
+      }
+
+      assertEquals(this.clock.instant().plusMillis(WAIT_PLAYER_READY_TIME), startTimeCaptor.getValue());
+    }
+  }
+
 }
