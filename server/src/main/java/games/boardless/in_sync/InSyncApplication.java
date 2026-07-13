7da0e920f331f1664e6570eef51a878640312135
poold3 <dallinpool@gmail.com>
2026-07-12 21:35:20 -0400

    Bad code.

--- server/src/main/java/games/boardless/in_sync/InSyncApplication.java
+++ server/src/main/java/games/boardless/in_sync/InSyncApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InSyncApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(InSyncApplication.class, args);
+    SpringApplication.run(InSyncApplication.class, args)
   }
 }
