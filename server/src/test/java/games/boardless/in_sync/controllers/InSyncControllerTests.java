ecee20b1a7a6a0a872f5dd5c475d3f083a7818d2
poold3 <dallinpool@gmail.com>
2026-07-12 21:34:39 -0400

    Failing test case.

--- server/src/test/java/games/boardless/in_sync/controllers/InSyncControllerTests.java
+++ server/src/test/java/games/boardless/in_sync/controllers/InSyncControllerTests.java
@@ -43,7 +43,7 @@ class InSyncControllerTests {
     this.mockMvc
         .perform(MockMvcRequestBuilders.post("/in-sync/game"))
         .andExpectAll(
-            MockMvcResultMatchers.status().isCreated(),
+            MockMvcResultMatchers.status().is4xxClientError(),
             MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON),
             MockMvcResultMatchers.jsonPath("$.gameCode").value(dto.gameCode()));
   }
