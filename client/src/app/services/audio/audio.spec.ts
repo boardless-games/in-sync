3a2bc084b03d33d10c52c1268bc90f38ec29b77f
poold3 <dallinpool@gmail.com>
2026-07-20 22:55:48 -0400

    Fixing audio service test cases.

--- client/src/app/services/audio/audio.spec.ts
+++ client/src/app/services/audio/audio.spec.ts
@@ -1,16 +1,26 @@
 import { TestBed } from "@angular/core/testing";
 
 import { AudioService } from "./audio";
+import { Mock } from "vitest";
+import { AudioFile } from "../../constants/AudioFile";
 
 describe("AudioService", () => {
   let service: AudioService;
+  let mockFetch: Mock<typeof fetch>;
 
   beforeEach(() => {
+    mockFetch = vi
+      .spyOn(globalThis, "fetch")
+      .mockImplementation(() => Promise.resolve(new Response()));
     TestBed.configureTestingModule({});
     service = TestBed.inject(AudioService);
   });
 
   it("should be created", () => {
     expect(service).toBeTruthy();
+    const audioFiles = Object.values(AudioFile);
+    for (let i = 0; i < audioFiles.length; ++i) {
+      expect(mockFetch).toHaveBeenNthCalledWith(i + 1, audioFiles[i]);
+    }
   });
 });
--- client/vitest-base.config.ts
+++ client/vitest-base.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    setupFiles: ["./vitest.setup.ts"]
+    setupFiles: ["./vitest.setup.ts"],
+    environment: "jsdom"
   }
 });
