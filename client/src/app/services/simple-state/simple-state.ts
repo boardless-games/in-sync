dd87751802ad666b55ccc7a89899914f1c1f9203
poold3 <dallinpool@gmail.com>
2026-08-15 20:55:11 -0400

    Working on simple state.

--- client/src/app/services/simple-state/simple-state.ts
+++ client/src/app/services/simple-state/simple-state.ts
@@ -31,3 +31,41 @@ export class SimpleState {
     ) as typeof this.state;
   }
 }
+
+/*
+import { Service } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
+
+@Service()
+export class SimpleState {
+  private readonly _state = {
+    keyboardEnabled: new BehaviorSubject(false),
+    darkMode: new BehaviorSubject("")
+  };
+
+  public readonly state: {
+    [Key in keyof typeof this._state]: (typeof this._state)[Key] extends BehaviorSubject<infer T>
+      ? {
+          get: Observable<T>;
+          set: (callback: (state: T) => T) => void;
+        }
+      : never;
+  };
+
+  constructor() {
+    const createPublicState = <K extends keyof typeof this._state>(key: K) => ({
+      get: this._state[key].asObservable(),
+      set: (callback: (state: (typeof this._state)[K] extends BehaviorSubject<infer T> ? T : never) => ((typeof this._state)[K] extends BehaviorSubject<infer T> ? T : never) => {
+        this._state[key].next(callback(this._state[key].value));
+      }
+    });
+    this.state = Object.fromEntries(
+      (Object.keys(this._state) as (keyof typeof this._state)[]).map((key) => {
+        return [key, createPublicState(key)];
+      })
+    ) as typeof this.state;
+  }
+}
+
+
+*/
